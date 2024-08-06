c3d0976529517e9bea7ef372933a017d5a745455
Diablood <91621645+dontlover2329r23@users.noreply.github.com>
2024-08-06 12:58:34 +0300

    Add files via upload

---
+++ Quizlet_version0_9/QuizCard.java
@@ -0,0 +1,21 @@
+package Quizlet_version0_9;
+
+import java.io.Serializable;
+
+public class QuizCard implements Serializable {
+    private String question;
+    private String answer;
+
+    public QuizCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
